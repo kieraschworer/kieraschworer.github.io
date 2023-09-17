e7a5ef68f6ffbe4b420703c3ef40c7429a660157
kieraschworer <130387827+kieraschworer@users.noreply.github.com>
2023-09-16 22:47:42 -0600

    Add files via upload

--- starbitescafe/javascript/contact.js
+++ starbitescafe/javascript/contact.js
@@ -1,26 +1,102 @@
-/*Frequently Asked Questions*/
 "use strict";
 
-const toggle = evt => {
-  const aElement = evt.currentTarget; //a element is labeled
-  const h2Element = aElement.parentNode; //h2 element, a's parentNode is labeled
-  const divElement = h2Element.nextElementSibling; //div, h2's sibling is labeled
-
-  //open
-  if (divElement.hasAttribute("class")) {
-    divElement.removeAttribute("class");
-  }
-  else {
-    divElement.className= "open"; //find .open class in css - .open class hides div
-  }
-}
+const $ = selector => document.querySelector(selector);
 
 document.addEventListener("DOMContentLoaded", () => {
-  //select all a tags in faqs and label group as aElements
-    const aElements = faqs.querySelectorAll("#faqs a");
 
-    //when clicked, toggle aElement
-    for (let aElement of aElements) {
-      aElement.addEventListener("click", toggle);
+  $("#submitmessage").addEventListener("click", () => {
+
+    const contactemail = $("#contactemail");
+    const commentsection = $("#commentsection");
+
+    let isValid = true;
+
+    if (contactemail.value == "")
+    {
+      contactemail.nextElementSibling.textContent = "Please enter an email to submit message!"
+      isValid = false;
+      contactemail.focus();
     }
+    else {
+      contactemail.nextElementSibling.textContent = "";
+      commentsection.focus();
+    }
+    if (commentsection.value == "")
+    {
+      commentsection.nextElementSibling.nextElementSibling.textContent = "Please enter comments to submit message!"
+      isValid = false;
+    }
+    if (isValid) {
+      $("#contactform").submit();
+    }
+  })
+
+  $("#joinbutton").addEventListener("click", () => {
+    
+    //get and label email and name input fields for javascript
+    const firstemail = $("#firstemail");
+    const secondemail = $("#secondemail");
+    const username = $("#username");
+
+    //Check for errors! (empty entries)
+    let isValid = true;
+
+    //If entry is empty, isValid is false and the form is not submitted. The nextElementSibling in HTML is <span>, and it will show the error message there.
+    if (firstemail.value == "")
+    {
+      firstemail.nextElementSibling.textContent = "Please enter your email!";
+      isValid = false;
+      firstemail.focus();
+    }
+    //If the entry has content, then it refers to "else", the <span> element remains empty, and no error is produced for this exception!
+    else {
+      firstemail.nextElementSibling.textContent = "";
+      secondemail.focus();
+    }
+
+    //Second email verification
+    if (secondemail.value == "") { 
+      secondemail.nextElementSibling.textContent = "This field is required...";
+      isValid = false;
+  }
+
+  else if (firstemail.value != secondemail.value) { 
+      secondemail.nextElementSibling.textContent = "Please confirm email addresses match!";
+      isValid = false;
+  }
+  else {
+      secondemail.nextElementSibling.textContent = "";
+      username.focus();
+  }
+    //If second email does not match first email, the error message is produced!
+
+    //User's name verification
+    if (username.value == "")
+    {
+      username.nextElementSibling.textContent = "Please enter your name!"
+      isValid = false;
+    }
+    else {
+      username.nextElementSibling.textContent = "";
+    }
+
+    //If isValid is true, then the email form is submitted! :D
+
+    if (isValid) {
+      $("#emailform").submit();
+    }
+
+
+  });
+
+  $("#clearbutton").addEventListener("click", () => {
+    
+    //To clear the form, javascript will change the HTML values to become empty ("").
+    $("#firstemail").value = "";
+    $("#secondemail").value = "";
+    $("#username").value = "";
+    //when form is cleared, it will focus on the first input field
+    $("#firstemail").focus();
+  })
+  $("#firstemail").focus(); 
 })
